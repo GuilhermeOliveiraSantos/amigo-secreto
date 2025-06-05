33030700a865f28507b729f5e3a130276fe9bdcc
Guilherme Oliveira <guibalbinos@gmail.com>
2025-06-05 17:17:04 -0300

    Ativar botão reiniciar

--- js/app.js
+++ js/app.js
@@ -39,3 +39,10 @@ function embaralha(lista) {
             [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+    document.getElementById('nome-amigo').value = '';
+}
